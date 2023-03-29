2258ac719a0a679f130d6c6263d78ade7ceaa254
Kartel Egor <dimurg34@gmail.com>
2023-03-29 17:44:23 +0300

    InitialCommit

---
+++ FromCSVtoJSON/src/main/java/CSVmain.java
@@ -0,0 +1,84 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+public class CSVmain {
+    static String[] employee1 = "1,John,Smith,USA,25".split(",");
+    static String[] employee2 = "2,Ivan,Petrov,RU,23".split(",");
+    static String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+    static String fileNameCSV = "data.csv";
+
+    public static void main(String[] args) {
+
+        createCSVFile(fileNameCSV);
+
+        List<Employee> list = parseCSV(columnMapping, fileNameCSV);
+
+        String json = listToJson(list);
+
+        writeString(json, "data.json");
+
+    }
+
+    //-----------------------------Создание CSV файла-----------------------------------------------------
+    public static void createCSVFile(String filePath) {
+        try (CSVWriter writer = new CSVWriter(new FileWriter(filePath))) {
+            writer.writeNext(employee1);
+            writer.writeNext(employee2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //------------------------------Формируем список сотрудников из CSV файла---------------------------
+    public static List<Employee> parseCSV(String[] mapping, String fileName) {
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader(fileName));
+
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(mapping);
+
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+
+            List<Employee> staff = csv.parse();
+
+            return staff;
+
+        } catch (IOException ex) {ex.printStackTrace();
+            return null;
+        }
+    }
+
+    //-----------------------------Преобразование в строку в формате JSON--------------------------
+    public static String listToJson(List<Employee> list) {
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        String json = gson.toJson(list, listType);
+        return json;
+    }
+
+    //-----------------------------Запись данных в файл JSON--------------------------
+    public static void writeString(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
+            file.write(json);
+            file.flush();
+        } catch (IOException ex) {ex.printStackTrace();}
+    }
+
+}
---
+++ FromXMLtoJSON/src/main/java/XMLmain.java
@@ -0,0 +1,159 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class XMLmain {
+    private static final String EMPLOYEE = "employee";
+    private static final String ID = "id";
+    private static final String FIRSTNAME = "firstName";
+    private static final String LASTNAME = "lastName";
+    private static final String COUNTRY = "county";
+    private static final String AGE = "age";
+
+    static String fileName = "data.xml";
+
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, IOException, SAXException {
+
+        createXMLFile(fileName);
+
+        List<Employee> list = parseXML("data.xml");
+
+        String json = CSVmain.listToJson(list);
+
+        CSVmain.writeString(json, "data2.json");
+
+    }
+
+    //-----------------------------------Создание и заполнение XML файла---------------------------------
+    public static void createXMLFile(String fileName) throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.newDocument();
+
+        Element staff = document.createElement("staff");
+        document.appendChild(staff);
+
+        Element employee1 = document.createElement(EMPLOYEE);
+        staff.appendChild(employee1);
+        Element employee2 = document.createElement(EMPLOYEE);
+        staff.appendChild(employee2);
+
+        Element id1 = document.createElement(ID);
+        id1.appendChild((document.createTextNode("1")));
+        Element id2 = document.createElement(ID);
+        id2.appendChild((document.createTextNode("2")));
+        employee1.appendChild(id1);
+        employee2.appendChild(id2);
+
+        Element firstName1 = document.createElement(FIRSTNAME);
+        firstName1.appendChild((document.createTextNode("John")));
+        Element firstName2 = document.createElement(FIRSTNAME);
+        firstName2.appendChild((document.createTextNode("Ivan")));
+        employee1.appendChild(firstName1);
+        employee2.appendChild(firstName2);
+
+        Element lastName1 = document.createElement(LASTNAME);
+        lastName1.appendChild((document.createTextNode("Smith")));
+        Element lastName2 = document.createElement(LASTNAME);
+        lastName2.appendChild((document.createTextNode("Petrov")));
+        employee1.appendChild(lastName1);
+        employee2.appendChild(lastName2);
+
+        Element country1 = document.createElement(COUNTRY);
+        country1.appendChild((document.createTextNode("USA")));
+        Element country2 = document.createElement(COUNTRY);
+        country2.appendChild((document.createTextNode("RU")));
+        employee1.appendChild(country1);
+        employee2.appendChild(country2);
+
+        Element age1 = document.createElement(AGE);
+        age1.appendChild((document.createTextNode("25")));
+        Element age2 = document.createElement(AGE);
+        age2.appendChild((document.createTextNode("23")));
+        employee1.appendChild(age1);
+        employee2.appendChild(age2);
+
+        DOMSource domSource = new DOMSource(document);
+        StreamResult streamResult = new StreamResult(new File(fileName));
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.transform(domSource, streamResult);
+    }
+
+    //------------------------------Формируем список сотрудников из XML файла---------------------------
+    public static List<Employee> parseXML(String filePath) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> employees = new ArrayList<>();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(filePath);
+
+        Node root = document.getDocumentElement();
+        NodeList nodeList = root.getChildNodes();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node emp = nodeList.item(i);
+
+            if(Node.ELEMENT_NODE == emp.getNodeType()) {
+                Employee employee = parseEmployee(nodeList.item(i));
+                employees.add(employee);
+            }
+        }
+        return employees;
+    }
+
+    //-----------------------------------Отдельный метод заполнения данными-----------------------------
+    public static Employee parseEmployee(Node elementNode) {
+        long id = 0;
+        String firstName = null;
+        String lastName = null;
+        String country = null;
+        int age = 0;
+
+        NodeList elementOfEmp = elementNode.getChildNodes();
+        for (int j = 0; j < elementOfEmp.getLength(); j++) {
+
+            switch (elementOfEmp.item(j).getNodeName()) {
+
+                case ID: {
+                    id = Long.valueOf(elementOfEmp.item(j).getTextContent());
+                    break;
+                }
+                case FIRSTNAME: {
+                    firstName = elementOfEmp.item(j).getTextContent();
+                    break;
+                }
+                case LASTNAME: {
+                    lastName = elementOfEmp.item(j).getTextContent();
+                    break;
+                }
+                case COUNTRY: {
+                    country = elementOfEmp.item(j).getTextContent();
+                    break;
+                }
+                case AGE: {
+                    age = Integer.valueOf(elementOfEmp.item(j).getTextContent());
+                    break;
+                }
+            }
+        }
+        return new Employee(id, firstName, lastName, country, age);
+    }
+
+}
